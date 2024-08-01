80e67e457f1336b85d17b9fe8a11abc2b68041fd
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-01 12:41:04 +0100

    Define variables and annotate them with the corresponding type

---
+++ 0x00-python_variable_annotation/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Define variables and annotate them with the corresponding type
+"""
+
+a: int = 1
+pi: float = 3.14
+
+i_understand_annotations: bool = True
+school: str = "Holberton"
