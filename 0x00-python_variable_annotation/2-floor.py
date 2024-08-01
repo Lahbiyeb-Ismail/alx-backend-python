460a62f3aa0f65efbebf474627d75466ab4e7d26
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-01 12:33:34 +0100

    Writing a type-annotated function floor which takes a float n as argument and returns the floor of the float.

---
+++ 0x00-python_variable_annotation/2-floor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+"""
+Function that returns the largest integer less than or equal to a given number.
+
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the largest integer less than or equal to a given number.
+
+    Args:
+      n (float): The number to be rounded down.
+
+    Returns:
+      int: The largest integer less than or equal to `n`.
+    """
+    return math.floor(n)
