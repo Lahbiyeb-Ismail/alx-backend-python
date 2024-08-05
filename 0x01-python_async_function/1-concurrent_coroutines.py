c3988fdfaa9dec170a83a579b8ca50cf797b14af
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-05 12:31:22 +0100

    add types

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,11 +5,12 @@ Function that waits for `n` random delays and returns them in sorted order
 """
 
 import asyncio
+from typing import List
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Waits for `n` random delays and returns them in sorted order.
 
