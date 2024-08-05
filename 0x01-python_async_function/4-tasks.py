a6ddaf0b57e5f4a65fd0b384d67ad0dc8c5f9d6e
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-05 13:01:03 +0100

    Function that waits for a random delay multiple timesand returns the sorted delays

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,11 +6,12 @@ and returns the sorted delays
 """
 
 import asyncio
+from typing import List
 
 task_wait_random = __import__("3-tasks").task_wait_random
 
 
-async def task_wait_n(n, max_delay):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Asynchronously waits for a random delay multiple
     times and returns the sorted delays.
@@ -22,6 +23,15 @@ async def task_wait_n(n, max_delay):
     Returns:
       List[float]: A list of delays in seconds, sorted in ascending order.
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays)
+
+    de = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
+
+    for i in range(1, len(de)):
+        key = de[i]
+        j = i - 1
+        while j >= 0 and key < de[j]:
+            de[j + 1] = de[j]
+            j -= 1
+        de[j + 1] = key
+
+    return de
