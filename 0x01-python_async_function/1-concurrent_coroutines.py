69782316079cb51e428e8e8a8fcdae6207fd4968
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-05 12:04:20 +0100

    Function that waits for  random delays and returns them in sorted order

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Function that waits for `n` random delays and returns them in sorted order
+"""
+
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n, max_delay):
+    """
+    Waits for `n` random delays and returns them in sorted order.
+
+    Args:
+      n (int): The number of delays to wait for.
+      max_delay (int): The maximum delay value in seconds.
+
+    Returns:
+      List[float]: A list of `n` random delays in seconds,
+      sorted in ascending order.
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
