428814d79223ebac11d2d2dedcb3ccc86c0c0cf5
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-07 12:52:28 +0100

    add return type for the measure_runtime function

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,7 +10,7 @@ import time
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Measures the runtime of the async_comprehension function.
 
@@ -18,12 +18,7 @@ async def measure_runtime():
       float: The total runtime in seconds.
     """
     start_time = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.perf_counter()
     total_runtime = end_time - start_time
     return total_runtime
