8ca07ccd99dde1a69cfab326beb0848c0eb6b6e4
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-07 12:47:23 +0100

    Asynchronous generator that yields random numbers between 0 and 10.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,7 +9,7 @@ import random
 from typing import Any, Generator
 
 
-async def async_generator() -> Generator[float, Any, None]:  # type: ignore
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous generator that yields random numbers between 0 and 10.
 
@@ -19,4 +19,4 @@ async def async_generator() -> Generator[float, Any, None]:  # type: ignore
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield (random.uniform(0, 10))
+        yield (random.random() * 10)
