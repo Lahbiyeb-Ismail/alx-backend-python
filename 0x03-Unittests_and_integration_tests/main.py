0784a2c7ced4578c8d731f6aabf140a5a46d9fda
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-24 16:34:46 +0100

    Test case to verify that accessing a nested map with invalid keys raises a KeyError

--- 0x03-Unittests_and_integration_tests/main.py
+++ 0x03-Unittests_and_integration_tests/main.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 access_nested_map = __import__("utils").access_nested_map
 
-nested_map = {"a": 1}
+# nested_map = {"a": 1}
+# path = ("a",)
+
+# print(access_nested_map(nested_map, path))
+
+# nested_map = {"a": {"b": 2}}
+# path = ("a",)
+
+# print(access_nested_map(nested_map, path))
+
+nested_map = {}
 path = ("a",)
 
 print(access_nested_map(nested_map, path))
 
-nested_map = {"a": {"b": 2}}
-path = ("a",)
+# nested_map = {"a": 1}
+# path = ("a", "b")
 
-print(access_nested_map(nested_map, path))
-
-nested_map = {"a": {"b": 2}}
-path = ("a", "b")
-
-print(access_nested_map(nested_map, path))
+# print(access_nested_map(nested_map, path))
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -36,3 +36,30 @@ class TestAccessNestedMap(TestCase):
         """
         result = access_nested_map(nested_map, keys)
         self.assertEqual(result, expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",)),
+            ({"a": 1}, ("a", "b")),
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, keys):
+        """
+        Test case to verify that accessing a nested map with
+        invalid keys raises a KeyError.
+
+        Args:
+          nested_map (dict): The nested map to be accessed.
+          keys (list): The list of keys to access the nested map.
+
+        Raises:
+          AssertionError: If the KeyError is not raised
+          or if the raised KeyError does not match the
+          expected KeyError.
+
+        Returns:
+          None
+        """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, keys)
+        self.assertEqual(f"KeyError('{keys[-1]}')", repr(e.exception))
