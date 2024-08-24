dd638232499adaff127f87914dd784e7f8d8d425
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-24 16:51:52 +0100

    add unit tests for the memoize function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,7 +8,7 @@ from unittest.mock import patch
 
 from parameterized import parameterized
 
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(TestCase):
@@ -97,3 +97,44 @@ class TestGetJson(TestCase):
         mock.assert_called_once()
 
         patcher.stop()
+
+
+class TestMemoize(TestCase):
+    """
+    This class contains unit tests for the `memoize` function
+    """
+
+    def test_memoize(self):
+        """
+        Test the memoize decorator.
+
+        This method tests the functionality of the memoize
+        decorator by creating a TestClass
+        with a method and a memoized property. The method
+        returns the value 42, and the
+        property calls the method. The test verifies that the
+        method is only called once
+        when accessing the property multiple times.
+
+        Args:
+          self: The instance of the test class.
+
+        Returns:
+          None
+        """
+
+        class TestClass:
+            """Test class"""
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
