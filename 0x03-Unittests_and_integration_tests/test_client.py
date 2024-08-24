22d48127beab8639a792a2006dc6974d9d983afb
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-24 17:11:21 +0100

    add unittest for the public_repos_url method from the GithubOrgClient class

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Class contains unit tests.
 """
 
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import PropertyMock, patch
 
 from parameterized import parameterized
 
@@ -30,3 +30,30 @@ class TestGithubOrgClient(TestCase):
         test_class = GithubOrgClient(input)
         test_class.org()
         mock.assert_called_once_with(f"https://api.github.com/orgs/{input}")
+
+    def test_public_repos_url(self):
+        """
+        Test case for the `test_public_repos_url` method.
+
+        This method tests the functionality of the
+        `_public_repos_url` property in the `GithubOrgClient` class.
+        It uses the `patch` function from the `unittest.mock`
+        module to mock the `org` property of the `GithubOrgClient` class.
+        The `org` property is replaced with a `PropertyMock`
+        object that returns a predefined payload dictionary.
+        The `GithubOrgClient` class is then instantiated with
+        a test organization name.
+        The `_public_repos_url` property is accessed and the
+        result is compared to the value of the "repos_url" key
+        in the payload dictionary.
+        The test passes if the result is equal to the
+        payload's "repos_url" value.
+
+        """
+        path = "client.GithubOrgClient.org"
+        with patch(path, new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient("test")
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
