8e7790c8f103a3023c493ab8ada7f1e0f2c44d42
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-24 17:29:03 +0100

    9. Integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -110,7 +110,6 @@ class TestIntegrationGithubOrgClient(TestCase):
         """
         Set up the test class before running any test cases.
         """
-        pass
         config = {
             "return_value.json.side_effect": [
                 cls.org_payload,
@@ -135,3 +134,17 @@ class TestIntegrationGithubOrgClient(TestCase):
         self.assertEqual(test_class.public_repos(), self.expected_repos)
         self.assertEqual(test_class.public_repos("XLICENSE"), [])
         self.mock.assert_called()
+
+    def test_public_repos_with_license(self):
+        """Integration test for public repos with License"""
+        test_class = GithubOrgClient("google")
+
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("XLICENSE"), [])
+        self.assertEqual(test_class.public_repos("apache-2.0"), self.apache2_repos)
+        self.mock.assert_called()
+
+    @classmethod
+    def tearDownClass(cls):
+        """A class method called after tests in an individual class have run"""
+        cls.get_patcher.stop()
